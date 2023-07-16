714422ade5e369dbd405e172d358a31e30b762b3
Beneduelle <vik9703@gmail.com>
2023-07-16 18:09:38 +0300

    Created environments, and user model. Added method login to authentication service

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environmetns';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>
+    (`${this.host}/user/login`, user,
+     {observe: 'response'});
+  }
 }
