a0065fcbf832ce79f1c0c671e14356a6b1c330bb
Beneduelle <vik9703@gmail.com>
2023-07-16 18:57:29 +0300

    Created method saveToken

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -33,4 +33,9 @@ export class AuthenticationService {
     localStorage.removeItem('token');
     localStorage.removeItem('users');
   }
+
+  public saveToken(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
 }
