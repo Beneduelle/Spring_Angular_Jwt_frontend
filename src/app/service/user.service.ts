be28aa1c94ed7d1df0f8846c4487843ebedf82d8
Beneduelle <vik9703@gmail.com>
2023-07-16 21:04:44 +0300

    Created methods: getUsers, addUser, updateuser, resetPassword, updateProfileImage, deleteUser in User service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environmetns';
+import { HttpClient, HttpErrorResponse, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<User[]|HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/list`);
+  }
+
+  public addUser(formData: FormData): Observable<User|HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User|HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/update`, formData);
+  }
+
+  public resetPassword(email: string): Observable<any|HttpErrorResponse> {
+    return this.http.get(`${this.host}/resetPassword/${email}`);
+  }
+
+  public updateProfileImage(formData: FormData): Observable<HttpEvent<User>|HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/updateProfileImage`,
+     formData,
+     {reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  public deleteUser(userId: number): Observable<any|HttpErrorResponse> {
+    return this.http.delete<any>(`${this.host}/user/delete/${userId}`);
+  }
 }
