c390c44e93bf41588c1d9f2ff9ecaa3319ca7eb7
Beneduelle <vik9703@gmail.com>
2023-07-21 21:29:26 +0300

    Added methods for adding new user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,6 +5,8 @@ import { UserService } from '../service/user.service';
 import { NotificationService } from '../service/notification.service';
 import { NotificationType } from '../enum/notification-type.enum';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +14,15 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
   private titleSubject = new BehaviorSubject<string>('Users');
   public titleAction$ = this.titleSubject.asObservable();
   public users: User[] = [];
   public refreshing: boolean = false;
   private subscriptions: Subscription[] = [];
   public selectedUser: User | undefined;
+  public fileName: string = "";
+  public profileImage: any;
 
   constructor(private userService: UserService,
     private notificationService: NotificationService) {
@@ -57,8 +62,38 @@ export class UserComponent implements OnInit {
 
   public onSelectUser(selectedUser: User): void {
     this.selectedUser = selectedUser;
-    document.getElementById('openUserInfo')?.click();
+    this.clickButton('openUserInfo');
   }
+
+  public onProfileImageChange(event: any): void {
+    this.fileName = event.files[0].name;
+    this.profileImage = event.files[0];
+    
+  }
+
+  public saveNewUser() {
+    this.clickButton('new-user-save');
+  }
+
+public onAddNewUser(userForm: NgForm): void {
+  const formData = this.userService.createUserFormData("", userForm.value, this.profileImage);
+  this.subscriptions.push(
+    this.userService.addUser(formData).subscribe(
+      (response: User|any) => {
+        this.clickButton('new-user-close');
+        this.getUsers(false);
+        this.fileName = "";
+        this.profileImage = null; // OK since profileImage is of type File | null
+        this.sendNotification(NotificationType.SUCCESS,
+          `${response.firstName} ${response.lastName} updated successfully`)
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+      }
+    )
+  )
+}
+
 
   private sendNotification(notificationType: NotificationType, message: string): void {
     if (message) {
@@ -66,6 +101,10 @@ export class UserComponent implements OnInit {
     } else {
       this.notificationService.notify(notificationType, 'AN ERROR OCCURED. PLEASE TRY AGAIN');
     }
+  }
+
+  private clickButton(buttonId: string): void {
+    document.getElementById(buttonId)?.click();
   }
 
   ngOnInit(): void {
